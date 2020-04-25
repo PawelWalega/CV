ac95c23ae36880c0c4df22db0374d7d022c03568
Pawel W-ga <PawelW-ga@users.noreply.github.com>
2020-04-25 16:41:07 +0200

    JavaScript working!!

--- info.js
+++ info.js
@@ -1,181 +1,48 @@
-// Add class info--about remove class info--skills info--edu
-	Jestem początkującym programistą webowym z ambicją stania się Full Stack Web
-	Developer’em. Lubię uczyć się logiki i zasad działania języków programowania
-	a także poznawać struktury zbudowanych na nich witryn. Uwielbiam także
-	planować, analizować i optymalizować już istniejące systemy. Tak w życiu jak
-	i w programowaniu cechuje mnie : minimalizm, dokładność, pracowitość oraz
-    przywiązywanie uwagi do funkcjonalności.
-    
-// Add class info--skills remove class info--about info--edu
+let el;
 
-<div class="skill"> <!--  Empty div to create 1 row gap --></div>
-<!-- HTML5 -->
-<div class="skill">
-    <div class="skill__info">
-        <svg class="skill__icon">
-            <use xlink:href="img/symbols.svg#icon-html5"></use>
-        </svg>
-        HTML5
-    </div>
-    
-    <div class="skill__rating">
-    <svg class="skill__icon">
-        <use xlink:href="img/symbols.svg#icon-star-full"></use>
-    </svg>
-    <svg class="skill__icon">
-        <use xlink:href="img/symbols.svg#icon-star-full"></use>
-    </svg>
-    <svg class="skill__icon">
-        <use xlink:href="img/symbols.svg#icon-star-full"></use>
-    </svg>
-    <svg class="skill__icon">
-        <use xlink:href="img/symbols.svg#icon-star-half"></use>
-    </svg>
-    <svg class="skill__icon">
-        <use xlink:href="img/symbols.svg#icon-star-empty"></use>
-    </svg>
-</div>
-</div>
+function experience(el) {
+	el.classList.remove('info--about');
+	el.classList.remove('info--skills');
+	el.classList.add('info--edu');
+}
+function skills(el) {
+	el.classList.remove('info--about');
+	el.classList.remove('info--edu');
+	el.classList.add('info--skills');
+}
+function about(el) {
+	el.classList.remove('info--skills');
+	el.classList.remove('info--edu');
+	el.classList.add('info--about');
+}
+function image(el) {
+	el.classList.remove('info--skills');
+	el.classList.remove('info--edu');
+	el.classList.remove('info--about');
+}
 
-<!-- CSS3 -->
-<div class="skill">
-<div class="skill__info">
-    <svg class="skill__icon">
-        <use xlink:href="img/symbols.svg#icon-css3"></use>
-    </svg>
-    CSS3
-</div>
+function showExp() {
+	el = document.getElementById('info');
+	el.innerHTML =
+		'<h2 class="heading-2">Edukacja:</h2><h3 class="heading-3"></h3><div class="info__bullet-point">Modern JavaScript bootcamp 2020 - W trakcie nauki</div><div class="info__bullet-point">Advanced CSS - SASS, Flexbox, GRID + animations (Udemy kurs)</div><div class="info__bullet-point">Responsive Blog with bootstrap - podstawy budowy systemu CMS przy pomocy PHP, MySQLi oraz Bootstrapa</div><div class="info__bullet-point">Pasja informatyki - Podstawy HTML, CSS, PHP, JavaScript, Bootstrap</div><br><h2 class="heading-2">Doświadczenie:</h2><br><h3 class="heading-3">TiGi Team Leader’s Support</h3><div class="info__date">01.2016-05.2019 Milton Keynes, Wielka Brytania</div><div class="info__bullet-point">organizacja pracy teamu goods in</div><div class="info__bullet-point">sprawdzanie nieścisłości produktów/raportowanie</div><div class="info__bullet-point">zarządzanie dostawami (inbound oraz outbound)</div><div class="info__bullet-point">szkolenie nowych pracowników</div><div class="info__bullet-point">przygotowywanie procedur</div></div>';
+	experience(el);
+}
 
-<div class="skill__rating">
-<svg class="skill__icon">
-    <use xlink:href="img/symbols.svg#icon-star-full"></use>
-</svg>
-<svg class="skill__icon">
-    <use xlink:href="img/symbols.svg#icon-star-full"></use>
-</svg>
-<svg class="skill__icon">
-    <use xlink:href="img/symbols.svg#icon-star-full"></use>
-</svg>
-<svg class="skill__icon">
-    <use xlink:href="img/symbols.svg#icon-star-empty"></use>
-</svg>
-<svg class="skill__icon">
-    <use xlink:href="img/symbols.svg#icon-star-empty"></use>
-</svg>
-</div>
-</div>
-<!-- SASS -->
-<div class="skill">
-<div class="skill__info">
-    <svg class="skill__icon">
-        <use xlink:href="img/symbols.svg#icon-sass"></use>
-    </svg>
-    SASS
-</div>
+function showSkills() {
+	el = document.getElementById('info');
+	el.innerHTML =
+		'<div class="skill"> <!--  Empty div to create 1 row gap --></div><!-- HTML5 --><div class="skill"><div class="skill__info"><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-html5"></use></svg>HTML5</div><div class="skill__rating"><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-star-full"></use></svg><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-star-full"></use></svg><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-star-full"></use></svg><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-star-half"></use></svg><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-star-empty"></use></svg></div></div><!-- CSS3 --><div class="skill"><div class="skill__info"><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-css3"></use></svg>CSS3</div><div class="skill__rating"><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-star-full"></use></svg><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-star-full"></use></svg><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-star-full"></use></svg><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-star-empty"></use></svg><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-star-empty"></use></svg></div></div><!-- SASS --><div class="skill"><div class="skill__info"><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-sass"></use></svg>SASS</div><div class="skill__rating"><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-star-full"></use></svg><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-star-full"></use></svg><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-star-full"></use></svg><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-star-empty"></use></svg><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-star-empty"></use></svg></div></div><!-- Java Script --><div class="skill"><div class="skill__info"><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-javascript"></use></svg>Java Script</div><div class="skill__rating"><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-star-full"></use></svg><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-star-full"></use></svg><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-star-empty"></use></svg><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-star-empty"></use></svg><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-star-empty"></use></svg></div></div><!-- Bootstrap 4 --><div class="skill"><div class="skill__info"><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-bootstrap"></use></svg>Bootstrap 4</div><div class="skill__rating"><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-star-full"></use></svg><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-star-full"></use></svg><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-star-full"></use></svg><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-star-half"></use></svg><svg class="skill__icon"><use xlink:href="img/symbols.svg#icon-star-empty"></use></svg></div></div><div class="skill"> <!--  Empty div to create 1 row gap --></div><div class="skill__others">Podstawy: <br>- PHP i MySQL <br>- Git VCS i Linux <br>- PSD to HTML <br>- Metodologia BEM <br>- CSS Grid + Flexbox <br><br>Język Angielski C1/C2</div>';
+	skills(el);
+}
 
-<div class="skill__rating">
-<svg class="skill__icon">
-    <use xlink:href="img/symbols.svg#icon-star-full"></use>
-</svg>
-<svg class="skill__icon">
-    <use xlink:href="img/symbols.svg#icon-star-full"></use>
-</svg>
-<svg class="skill__icon">
-    <use xlink:href="img/symbols.svg#icon-star-full"></use>
-</svg>
-<svg class="skill__icon">
-    <use xlink:href="img/symbols.svg#icon-star-empty"></use>
-</svg>
-<svg class="skill__icon">
-    <use xlink:href="img/symbols.svg#icon-star-empty"></use>
-</svg>
-</div>
-</div>
-<!-- Java Script -->
-<div class="skill">
-<div class="skill__info">
-    <svg class="skill__icon">
-        <use xlink:href="img/symbols.svg#icon-javascript"></use>
-    </svg>
-    Java Script
-</div>
+function showAbout() {
+	el = document.getElementById('info');
+	el.innerHTML =
+		'Jestem początkującym programistą webowym z ambicją stania się Full Stack Web Developer’em. Lubię uczyć się logiki i zasad działania języków programowania a także poznawać struktury zbudowanych na nich witryn. Uwielbiam także planować, analizować i optymalizować już istniejące systemy. Tak w życiu jak i w programowaniu cechuje mnie : minimalizm, dokładność, pracowitość oraz przywiązywanie uwagi do funkcjonalności.';
+	about(el);
+}
 
-<div class="skill__rating">
-<svg class="skill__icon">
-    <use xlink:href="img/symbols.svg#icon-star-full"></use>
-</svg>
-<svg class="skill__icon">
-    <use xlink:href="img/symbols.svg#icon-star-full"></use>
-</svg>
-<svg class="skill__icon">
-    <use xlink:href="img/symbols.svg#icon-star-empty"></use>
-</svg>
-<svg class="skill__icon">
-    <use xlink:href="img/symbols.svg#icon-star-empty"></use>
-</svg>
-<svg class="skill__icon">
-    <use xlink:href="img/symbols.svg#icon-star-empty"></use>
-</svg>
-</div>
-</div>
-<!-- Bootstrap 4 -->
-<div class="skill">
-<div class="skill__info">
-    <svg class="skill__icon">
-        <use xlink:href="img/symbols.svg#icon-bootstrap"></use>
-    </svg>
-    Bootstrap 4
-</div>
-
-<div class="skill__rating">
-<svg class="skill__icon">
-    <use xlink:href="img/symbols.svg#icon-star-full"></use>
-</svg>
-<svg class="skill__icon">
-    <use xlink:href="img/symbols.svg#icon-star-full"></use>
-</svg>
-<svg class="skill__icon">
-    <use xlink:href="img/symbols.svg#icon-star-full"></use>
-</svg>
-<svg class="skill__icon">
-    <use xlink:href="img/symbols.svg#icon-star-half"></use>
-</svg>
-<svg class="skill__icon">
-    <use xlink:href="img/symbols.svg#icon-star-empty"></use>
-</svg>
-</div>
-</div>
-
-<div class="skill"> <!--  Empty div to create 1 row gap --></div>
-
-<div class="skill__others">
-
-Podstawy: <br>
-- PHP i MySQL <br>
-- Git VCS i Linux <br>
-- PSD to HTML <br>
-- Metodologia BEM <br>
-- CSS Grid + Flexbox <br>
-<br>
-Język Angielski C1/C2
-</div>    
-
-// Add class info--edu remove class info--about info--skills
-<h2 class="heading-2">Edukacja:</h2>
-        <h3 class="heading-3"></h3>
-            <div class="info__bullet-point">Modern JavaScript bootcamp 2020 - W trakcie nauki</div>
-            <div class="info__bullet-point">Advanced CSS - SASS, Flexbox, GRID + animations (Udemy kurs)</div>
-            <div class="info__bullet-point">Responsive Blog with bootstrap - podstawy budowy systemu CMS przy pomocy PHP, MySQLi oraz Bootstrapa</div>
-            <div class="info__bullet-point">Pasja informatyki - Podstawy HTML, CSS, PHP, JavaScript, Bootstrap</div>
-            <br>
-
-        <h2 class="heading-2">Doświadczenie:</h2>
-<br>
-            <h3 class="heading-3">TiGi Team Leader’s Support</h3>
-            <div class="info__date">01.2016-05.2019 Milton Keynes, Wielka Brytania</div>
-            <div class="info__bullet-point">organizacja pracy teamu goods in</div>
-            <div class="info__bullet-point">sprawdzanie nieścisłości produktów/raportowanie</div>
-            <div class="info__bullet-point">zarządzanie dostawami (inbound oraz outbound)</div>
-            <div class="info__bullet-point">szkolenie nowych pracowników</div>
-            <div class="info__bullet-point">przygotowywanie procedur</div>
-        </div>
+function showImage() {
+	document.getElementById('#info').html(``);
+	image(el);
+}
