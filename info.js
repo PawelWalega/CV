97311ea309b18b45973d27725112f00c8a06b184
Pawel W-ga <PawelW-ga@users.noreply.github.com>
2020-04-25 13:57:31 +0200

    Finished styling skills and about

---
+++ info.js
@@ -0,0 +1,161 @@
+// Add class info--about remove class info--skills info--edu
+	Jestem początkującym programistą webowym z ambicją stania się Full Stack Web
+	Developer’em. Lubię uczyć się logiki i zasad działania języków programowania
+	a także poznawać struktury zbudowanych na nich witryn. Uwielbiam także
+	planować, analizować i optymalizować już istniejące systemy. Tak w życiu jak
+	i w programowaniu cechuje mnie : minimalizm, dokładność, pracowitość oraz
+    przywiązywanie uwagi do funkcjonalności.
+    
+// Add class info--skills remove class info--about info--edu
+
+<div class="skill"> <!--  Empty div to create 1 row gap --></div>
+<!-- HTML5 -->
+<div class="skill">
+    <div class="skill__info">
+        <svg class="skill__icon">
+            <use xlink:href="img/symbols.svg#icon-html5"></use>
+        </svg>
+        HTML5
+    </div>
+    
+    <div class="skill__rating">
+    <svg class="skill__icon">
+        <use xlink:href="img/symbols.svg#icon-star-full"></use>
+    </svg>
+    <svg class="skill__icon">
+        <use xlink:href="img/symbols.svg#icon-star-full"></use>
+    </svg>
+    <svg class="skill__icon">
+        <use xlink:href="img/symbols.svg#icon-star-full"></use>
+    </svg>
+    <svg class="skill__icon">
+        <use xlink:href="img/symbols.svg#icon-star-half"></use>
+    </svg>
+    <svg class="skill__icon">
+        <use xlink:href="img/symbols.svg#icon-star-empty"></use>
+    </svg>
+</div>
+</div>
+
+<!-- CSS3 -->
+<div class="skill">
+<div class="skill__info">
+    <svg class="skill__icon">
+        <use xlink:href="img/symbols.svg#icon-css3"></use>
+    </svg>
+    CSS3
+</div>
+
+<div class="skill__rating">
+<svg class="skill__icon">
+    <use xlink:href="img/symbols.svg#icon-star-full"></use>
+</svg>
+<svg class="skill__icon">
+    <use xlink:href="img/symbols.svg#icon-star-full"></use>
+</svg>
+<svg class="skill__icon">
+    <use xlink:href="img/symbols.svg#icon-star-full"></use>
+</svg>
+<svg class="skill__icon">
+    <use xlink:href="img/symbols.svg#icon-star-empty"></use>
+</svg>
+<svg class="skill__icon">
+    <use xlink:href="img/symbols.svg#icon-star-empty"></use>
+</svg>
+</div>
+</div>
+<!-- SASS -->
+<div class="skill">
+<div class="skill__info">
+    <svg class="skill__icon">
+        <use xlink:href="img/symbols.svg#icon-sass"></use>
+    </svg>
+    SASS
+</div>
+
+<div class="skill__rating">
+<svg class="skill__icon">
+    <use xlink:href="img/symbols.svg#icon-star-full"></use>
+</svg>
+<svg class="skill__icon">
+    <use xlink:href="img/symbols.svg#icon-star-full"></use>
+</svg>
+<svg class="skill__icon">
+    <use xlink:href="img/symbols.svg#icon-star-full"></use>
+</svg>
+<svg class="skill__icon">
+    <use xlink:href="img/symbols.svg#icon-star-empty"></use>
+</svg>
+<svg class="skill__icon">
+    <use xlink:href="img/symbols.svg#icon-star-empty"></use>
+</svg>
+</div>
+</div>
+<!-- Java Script -->
+<div class="skill">
+<div class="skill__info">
+    <svg class="skill__icon">
+        <use xlink:href="img/symbols.svg#icon-javascript"></use>
+    </svg>
+    Java Script
+</div>
+
+<div class="skill__rating">
+<svg class="skill__icon">
+    <use xlink:href="img/symbols.svg#icon-star-full"></use>
+</svg>
+<svg class="skill__icon">
+    <use xlink:href="img/symbols.svg#icon-star-full"></use>
+</svg>
+<svg class="skill__icon">
+    <use xlink:href="img/symbols.svg#icon-star-empty"></use>
+</svg>
+<svg class="skill__icon">
+    <use xlink:href="img/symbols.svg#icon-star-empty"></use>
+</svg>
+<svg class="skill__icon">
+    <use xlink:href="img/symbols.svg#icon-star-empty"></use>
+</svg>
+</div>
+</div>
+<!-- Bootstrap 4 -->
+<div class="skill">
+<div class="skill__info">
+    <svg class="skill__icon">
+        <use xlink:href="img/symbols.svg#icon-bootstrap"></use>
+    </svg>
+    Bootstrap 4
+</div>
+
+<div class="skill__rating">
+<svg class="skill__icon">
+    <use xlink:href="img/symbols.svg#icon-star-full"></use>
+</svg>
+<svg class="skill__icon">
+    <use xlink:href="img/symbols.svg#icon-star-full"></use>
+</svg>
+<svg class="skill__icon">
+    <use xlink:href="img/symbols.svg#icon-star-full"></use>
+</svg>
+<svg class="skill__icon">
+    <use xlink:href="img/symbols.svg#icon-star-half"></use>
+</svg>
+<svg class="skill__icon">
+    <use xlink:href="img/symbols.svg#icon-star-empty"></use>
+</svg>
+</div>
+</div>
+
+<div class="skill"> <!--  Empty div to create 1 row gap --></div>
+
+<div class="skill__others">
+
+Podstawy: <br>
+- PHP i MySQL <br>
+- Git VCS i Linux <br>
+- PSD to HTML <br>
+- Metodologia BEM <br>
+- CSS Grid + Flexbox <br>
+<br>
+Język Angielski C1/C2
+</div>    
