5f3735d4f0a42e50ae88eb25978211ccbfba5f21
Pawel W-ga <PawelW-ga@users.noreply.github.com>
2020-04-26 16:08:07 +0200

    Made image responsive

--- timer.js
+++ timer.js
@@ -1,16 +1,17 @@
 function odliczanie() {
-	var dzisiaj = new Date();
+	let dzisiaj = new Date();
 
-	var godzina = dzisiaj.getHours();
+	let godzina = dzisiaj.getHours();
 	if (godzina < 10) godzina = '0' + godzina;
 
-	var minuta = dzisiaj.getMinutes();
+	let minuta = dzisiaj.getMinutes();
 	if (minuta < 10) minuta = '0' + minuta;
 
-	var sekunda = dzisiaj.getSeconds();
+	let sekunda = dzisiaj.getSeconds();
 	if (sekunda < 10) sekunda = '0' + sekunda;
 
-	document.getElementById('zegar').innerHTML = godzina + ':' + minuta + ':' + sekunda;
+	document.getElementById('clock').innerHTML =
+		godzina + ':' + minuta + ':' + sekunda;
 
 	setTimeout('odliczanie()', 1000);
 }
